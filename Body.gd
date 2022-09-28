1d54cdb302615d958f01da5e65613ab73e257936
harvzor <harveyjwilliams@aol.co.uk>
2022-09-29 00:08:16 +0200

    Realistic gravity.

--- Body.gd
+++ Body.gd
@@ -1,22 +1,18 @@
-extends MeshInstance2D
+extends RigidBody2D
 
-var gravity = 6.6743 * pow(10,-11)
-export var mass = 0.0
+#var gravity = 6.6743 * pow(10,-11)
+var gravity = 6.6743 * pow(10, 1)
+#export var mass = 0.0
 var velocity = Vector2.ZERO
-export var sun = false
+var group = "bodies"
 
 func _ready():
-	if sun:
-		$OmniLight.energy = 10
-
-func _process(delta):
-	velocity += acceleration()
-	position += velocity
+	self.add_to_group(group)
 
 func acceleration():
 	var acceleration = Vector2.ZERO
 	for body in get_parent().get_children():
-		if body.is_in_group("physics"):
+		if body.is_in_group(group):
 			if body != self:
 				var acc = gravity(body.mass, body.position)/mass
 				acceleration += acc
@@ -28,3 +24,6 @@ func gravity(m, position2):
 	var vector = (position2 - position).normalized()
 	force *= vector
 	return force
+
+func _integrate_forces(state):
+	self.apply_impulse(Vector2.ZERO, acceleration())
