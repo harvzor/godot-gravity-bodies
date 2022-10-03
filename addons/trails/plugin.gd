6ebeed6f47fe7639ff11aabaefe8c394a258c0c2
harvzor <harveyjwilliams@aol.co.uk>
2022-10-03 14:14:37 +0200

    Some renames and nicer paths.

--- addons/trails/plugin.gd
+++ addons/trails/plugin.gd
@@ -3,9 +3,6 @@ extends EditorPlugin
 
 var custom_type = "Trail2D"
 
-func get_name(): 
-	return "Trails"
-
 func _enter_tree():
 	add_custom_type(custom_type, "Line2D", preload("2DTrail.gd"), preload("trails2d.png"))
 func _exit_tree():
